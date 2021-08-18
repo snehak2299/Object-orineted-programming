6036689d6bee69ea9775766bba47a6fa69440fef
snehak2299 <skanase2299@gmail.com>
2021-08-18 21:22:50 +0530

    [sneha]:added inventory part to calculate amount

---
+++ InventoryData.java
@@ -0,0 +1,18 @@
+package com.bridglab.objectoriented;
+
+public class InventoryData {
+	public String name;
+	public final double weight;
+	public final double price;
+
+	public InventoryData(String name, double weight, double price) {
+		super();
+		this.name = name;
+		this.weight = weight;
+		this.price = price;
+	}
+	public void total() {
+		System.out.println("inventory "+name+" is "+weight+" kg "+price+" Rupees");
+	}
+
+}
